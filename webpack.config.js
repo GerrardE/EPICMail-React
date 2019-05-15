954a4b3ca2d8c5d21a4cd18e0f15f4f967bbe6d4
GerrardE <ezeugwagerrard@gmail.com>
2019-05-15 17:35:11 +0100

    ch(setup): Set up react app - Install project dependencies and dev deps
    [Delivers #166034467]

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'app_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+} 
